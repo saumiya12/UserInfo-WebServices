cab712a664f5afaddbc8c7c3459b7d3bcf360422
Saumiya.Vairamourty <saumiya.vairamourty@sap.com>
2021-02-27 20:23:37 +0530

    Revert "User Web Service"
    This reverts commit ed0b143fe8460764bbc67888b0d7a2766d88af15.

--- src/main/java/app/ws/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package app.ws.service;
-
-import app.ws.model.request.UserDetailsRequestModel;
-import app.ws.model.response.UserRest;
-
-public interface UserService {
-	UserRest createUser(UserDetailsRequestModel userDetails);
-}
